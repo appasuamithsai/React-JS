58a6f49264475d0864059f8ce801bfde52d9b624
appasuamithsai <amithsai.appasu@gmail.com>
2022-01-01 12:03:02 +0530

    latest-commit

---
+++ typescript/practice1.js
@@ -0,0 +1,24 @@
+var hello = "Hello World";
+var user = {
+    name: "amith",
+    id: 146
+};
+var s = {
+    name: "ajith",
+    id: 120
+};
+var user1 = {
+    name: "nithin",
+    id: 21
+};
+console.log(user1.id);
+var emp1 = {
+    name: "ram",
+    dept: "CSE"
+};
+console.log(emp1);
+var emp2 = {
+    name: "SHam",
+    dept: "EEE"
+};
+console.log(emp2);
---
+++ typescript/practice1.ts
@@ -0,0 +1,45 @@
+var hello : string="Hello World";
+
+
+//Interface Declaration
+interface User{
+    name:string;
+    id:number;
+}
+
+var user:User={
+    name:"amith",
+    id:146,
+};
+
+var s:User={
+    name:"ajith",
+    id:120,
+};
+
+
+var user1:User={
+    name:"nithin",
+    id:21,
+};
+console.log(user1.id);
+
+interface Employee{
+    name:string;
+    dept:string;
+}
+
+var emp1:Employee={
+    name:"ram",
+    dept:"CSE",
+};
+
+console.log(emp1);
+
+var emp2:Employee={
+    name:"SHam",
+    dept:"EEE",
+};
+
+ console.log(emp2);
+ 
